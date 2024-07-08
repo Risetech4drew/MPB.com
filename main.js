6ffd18ce177c1aad0ac8ad1e3301f8ec165d4297
Risetech4drew <kanyinjiandy2@gmail.com>
2024-07-08 15:11:17 +0200

    made the header stick to the top when user scrolls down the page

--- main.js
+++ main.js
@@ -66,6 +66,28 @@
       }
     }
   });
+  // easy on scroll helper function
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+  // stick header on top on scroll
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop;
+    let nextElement = selectHeader.nextElementSibling;
+    const headerFixed = () => {
+      if (headerOffset - window.scrollY <= 0) {
+        selectHeader.classList.add("fixed-top");
+        // nextElement.classList.add("scrolled-offset");
+      } else {
+        selectHeader.classList.remove("fixed-top");
+        // nextElement.classList.remove("scrolled-offset");fixed-top
+      }
+    };
+    window.addEventListener("load", headerFixed);
+    onscroll(document, headerFixed);
+  }
+
   class TypeWritter {
     constructor(textElement, words, wait = 3000) {
       this.textElement = textElement;
