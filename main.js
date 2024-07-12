05482618787c4c833181a061d43df99297c24193
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-07-12 19:21:35 +0200

    placed required text content in catalogue cards

--- main.js
+++ main.js
@@ -216,7 +216,6 @@
   // swiper
   new Swiper(".itemSwiper", {
     slidesPerView: 2,
-    spaceBetween: 30,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
