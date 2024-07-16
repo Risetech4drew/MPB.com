0d066a26ba63555170355b839a9b9ee08c7db3e0
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-07-16 21:58:15 +0200

    changed the dispaly property of services container to grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundImage: {
         hero: "url('/assets/imgs/PPE.jpg')",
       },
+      gridTemplateColumns: {
+        "service-cards": "repeat(auto-fill,minmax(15em,1fr))",
+      },
     },
     container: {
       center: true,
