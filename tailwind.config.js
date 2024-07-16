4227ea8585a656601f19014e8b79f648e89d628b
Andrew kanyinji <kanyinjiandy2@gmail.com>
2024-07-16 09:45:35 +0200

    changed on how the custom background is added to the hero section and used tailwind media query utility class to to bg-fixed on mobile devices by setting bg-scroll on mobile & tablets and bg-fixed on larger screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('/assets/imgs/PPE.jpg')",
+      },
+    },
     container: {
       center: true,
     },
